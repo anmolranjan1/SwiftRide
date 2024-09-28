b030b9f82329d03462a44e270b7ff13f2bd0688c
Anmol Ranjan <anmolranjan1@gmail.com>
2024-09-28 20:17:35 +0530

    Updated Location Feature

---
+++ SwiftRide/Managers/LocationManager.swift
@@ -0,0 +1,27 @@
+//
+//  LocationManager.swift
+//  SwiftRide
+//
+//  Created by Anmol Ranjan on 28/09/24.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
