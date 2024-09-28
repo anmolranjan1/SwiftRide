7e16f331de4862e3653816010223600e41a87829
Anmol Ranjan <anmolranjan1@gmail.com>
2024-09-28 19:16:45 +0530

    Initial Commit

---
+++ SwiftRide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftRide
+//
+//  Created by Anmol Ranjan on 28/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftRide/SwiftRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRideApp.swift
+//  SwiftRide
+//
+//  Created by Anmol Ranjan on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
