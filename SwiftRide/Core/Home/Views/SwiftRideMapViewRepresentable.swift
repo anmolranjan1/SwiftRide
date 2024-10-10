ff99aeaafbaa494a10d20239037fdbe0e1e2d567
Anmol Ranjan <anmolranjan1@gmail.com>
2024-10-10 13:36:03 +0530

    Selection Location Functionality Added

--- SwiftRide/Core/Home/Views/SwiftRideMapViewRepresentable.swift
+++ SwiftRide/Core/Home/Views/SwiftRideMapViewRepresentable.swift
@@ -11,6 +11,7 @@ import MapKit
 struct SwiftRideMapViewRepresentable: UIViewRepresentable {
     let mapView = MKMapView()
     let locationManager = LocationManager()
+    @EnvironmentObject var locationViewModel: LocationSearchViewModel
     
     func makeUIView(context: Context) -> some UIView {
         mapView.delegate = context.coordinator
@@ -22,7 +23,9 @@ struct SwiftRideMapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        
+        if let selectedLocation = locationViewModel.selectedLocation {
+            print("\(selectedLocation)")
+        }
     }
     
     func makeCoordinator() -> MapCoordinator {
